58f0ae19089db33026be161f5ce5e67207c79237
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-09-16 08:36:46 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+const Selection = ({ color }) => {
+  const [bgColor, setBgColor] = useState("");
+
+  useEffect(() => {
+    setBgColor(color);
+  }, [color]);
+
+  return (
+    <div
+      className="fix-box"
+      style={{
+        width: "100px",
+        height: "100px",
+        border: "1px solid black",
+        backgroundColor: bgColor,
+      }}
+    ></div>
+  );
+};
+
+export default Selection;
 
